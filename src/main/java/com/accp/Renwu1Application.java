a367cc5e1fdd92657beef86c04a27c82c00a17f2
lirui <liruirui@example.com>
2020-01-02 10:04:43 +0800

    第一次提交代码库

---
+++ src/main/java/com/accp/Renwu1Application.java
@@ -0,0 +1,13 @@
+package com.accp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Renwu1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Renwu1Application.class, args);
+	}
+
+}
